a84610830adaa82313226510bb4a861c85d11b8e
Tamandani Nsiku <sebastiangooddays@gmail.com>
2025-04-28 21:45:45 +0200

    Updated JS on hero images

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hero = document.querySelector('.hero');
-    let position = 0;
-  
-    function animateBackground() {
-      position += 0.1; // speed of movement (you can adjust this)
-      hero.style.backgroundPosition = `${position}px center`;;
-      requestAnimationFrame(animateBackground); // keeps it moving
-    }
-  
-    animateBackground();
-  });
+const heroSection = document.querySelector('.hero');
+
+const images = [
+  "https://mubas.ac.mw/images/slider/slide1.jpg",
+  "https://mubas.ac.mw/images/slider/slide2.jpg",
+  "https://mubas.ac.mw/images/slider/slide3.jpg"
+];
+
+let currentIndex = 0;
+
+setInterval(() => {
+  currentIndex = (currentIndex + 2) % images.length;
+  heroSection.style.backgroundImage = `url(${images[currentIndex]})`;
+}, 3000); // 5 seconds
